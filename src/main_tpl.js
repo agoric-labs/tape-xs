b504933571f0feb3e84970f48ce598b3535ea93d
Dan Connolly <dckc@madmode.com>
2019-12-23 23:10:06 -0600

    provide setTimeout to tests such as eventual-send

--- src/main_tpl.js
+++ src/main_tpl.js
@@ -1,8 +1,25 @@
+/* global Compartment, trace */
+import Timer from 'timer';
+
+const harden = x => Object.freeze(x, true);
+
 export default async function main() {
   trace('in main.\n');
-  const c0 = new Compartment('tape-promise/tape');
-  trace('c0\n');
   trace(JSON.stringify(Object.keys(Compartment.map), null, 2));
-  const c1 = new Compartment('@agoric/harden');
+  const { setTimeout } = makeTimer(Timer);
+  const modMap = { ...Compartment.map };
+  delete modMap['timer']; // ISSUE: should whitelist
+  const c1 = new Compartment('test/test', { setTimeout }, modMap);
   trace('c1\n');
+}
+
+function makeTimer(Timer) {
+  return harden({
+    setImmediate(callback) {
+      Timer.set(callback);
+    },
+    setTimeout(callback, delay) {
+      Timer.set(callback, delay);
+    }
+  });
 }
